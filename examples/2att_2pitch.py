0a030a8ee10f54df5db34e78ac4494db1694f02c
finani <10ezenfinani@gmail.com>
2025-03-03 22:42:05 +0900

    rebuild AttitudeController

---
+++ examples/2att_2pitch.py
@@ -0,0 +1,113 @@
+import hydra
+import os
+from pathlib import Path
+import torch
+from tqdm import trange
+
+from omni_drones import init_simulation_app
+from tensordict import TensorDict
+
+
+@hydra.main(config_path=os.path.dirname(__file__), config_name=Path(__file__).stem)
+def main(cfg):
+    app = init_simulation_app(cfg)
+
+    # due to the design of Isaac Sim, these imports are only available
+    # after the SimulationApp instance is created
+    from omni_drones.envs.isaac_env import IsaacEnv
+    from omni_drones.robots.assets import Multirotor, get_robot_cfg
+
+    from isaaclab.scene import InteractiveSceneCfg
+    from isaaclab.assets import AssetBaseCfg
+    from isaaclab.terrains import TerrainImporterCfg
+    import isaaclab.sim as sim_utils
+
+    class MyEnv(IsaacEnv):
+        def __init__(self, cfg):
+            super().__init__(cfg)
+            # the `__init__` method invokes `_design_scene` to create the scene
+            # after that, all the entities created are managed by `self.scene`
+            print(self.scene)
+
+            # let's get the drone entity
+            self.drone: Multirotor = self.scene["drone"]
+            self.default_init_state = self.drone.data.default_root_state.clone()
+
+            self.drone_mass = self.drone.root_physx_view.get_masses().sum(-1, keepdim=True).to(self.device)
+            self.drone_mass = self.drone_mass.flatten(0, -2)[0]
+
+            self.target_roll = torch.zeros(self.drone.shape, device=self.device)
+            self.target_roll[:] = cfg.goal[0] / 180.0 * torch.pi
+            self.target_pitch = torch.zeros(self.drone.shape, device=self.device)
+            self.target_pitch[:] = cfg.goal[1] / 180.0 * torch.pi
+            self.target_yaw = torch.zeros(self.drone.shape, device=self.device)
+            self.target_yaw[:] = cfg.goal[2] / 180.0 * torch.pi
+
+            # default_init_pos = self.drone.data.root_pos_w
+            # self.target_z = torch.ones(self.drone.shape, device=self.device) * default_init_pos[:, 2]
+            self.target_z = torch.zeros(self.drone.shape, device=self.device)
+            self.target_z[:] = cfg.goal[3]
+
+            self.resolve_specs()
+
+        def _design_scene(self):
+            # the scene is created from a SceneCfg object in a declarative way
+            # see the docstring of `InteractiveSceneCfg` for more details
+            class SceneCfg(InteractiveSceneCfg):
+                terrain = TerrainImporterCfg(
+                    prim_path="/World/ground",
+                    terrain_type="plane",
+                    collision_group=-1,
+                )
+                # lights
+                light = AssetBaseCfg(
+                    prim_path="/World/light",
+                    spawn=sim_utils.DistantLightCfg(color=(0.75, 0.75, 0.75), intensity=3000.0),
+                )
+                sky_light = AssetBaseCfg(
+                    prim_path="/World/skyLight",
+                    spawn=sim_utils.DomeLightCfg(color=(0.13, 0.13, 0.13), intensity=1000.0),
+                )
+
+                drone = get_robot_cfg(cfg.robot_name).replace(
+                    prim_path="{ENV_REGEX_NS}/Robot",
+                )
+
+            return SceneCfg(num_envs=cfg.num_envs, env_spacing=2.5)
+
+        def _reset_idx(self, env_ids: torch.Tensor):
+            # since we have multiple parallel environments
+            # the environment offset is added to the initial state
+            init_state = self.default_init_state[env_ids]
+            init_state[:, :3] += self.scene.env_origins[env_ids]
+
+            self.drone.write_root_state_to_sim(init_state, env_ids)
+
+    env = MyEnv(cfg)
+
+    def policy(tensordict: TensorDict):
+        target_pitch = env.target_pitch.unsqueeze(1)
+        target_roll = env.target_roll.unsqueeze(1)
+        target_yaw = env.target_yaw.unsqueeze(1)
+        target_z = env.target_z.unsqueeze(1)
+        action = torch.cat([target_pitch, target_roll, target_yaw, target_z], dim=-1)
+        tensordict["agents", "action"] = action
+        # print(f"{tensordict['agents', 'action']=}")
+        return tensordict
+
+    def rollout(env: IsaacEnv):
+        data_ = env.reset()
+        result = []
+        for _ in trange(env.max_episode_length):
+            data_ = policy(data_)
+            data, data_ = env.step_and_maybe_reset(data_)
+            result.append(data)
+        return torch.stack(result)
+
+    while app.is_running():
+        rollout(env)
+    app.close()
+
+
+if __name__ == "__main__":
+    main()
--- omni_drones/envs/mdp/action.py
+++ omni_drones/envs/mdp/action.py
@@ -242,3 +242,176 @@ class LeePositionController(ActionFunc):
             color=(0., 0., 0., .5)
         )
 
+
+class AttitudeController(ActionFunc):
+
+    def __init__(
+        self,
+        env: "IsaacEnv",
+        asset_name: str,
+        z_mode: str,
+        yaw_mode: str,
+        inertia: Tuple[float, float, float], # TODO: calculate inertia
+        ang_rate_gain: Tuple[float, float, float],
+        attitude_gain: Tuple[float, float, float],
+        vel_gain: Tuple[float, float, float] = (0.0, 0.0, 0.0),
+        z_gain: float = 0.0,
+    ):
+        super().__init__(env)
+        self.asset: Multirotor = self.env.scene[asset_name]
+        self.rotor: Rotor = self.asset.actuators["rotor"]
+        self.z_mode = z_mode
+        self.yaw_mode = yaw_mode
+
+        self.action_dim = 4
+
+        rotor_pos_w = self.asset.data.body_pos_w[0, self.rotor.body_ids, :]
+        rotor_pos_b = quat_rotate_inverse(
+            self.asset.data.root_quat_w[0].unsqueeze(0),
+            rotor_pos_w - self.asset.data.root_pos_w[0].unsqueeze(0)
+        )
+
+        arm_lengths = rotor_pos_b.norm(dim=-1)
+        rotor_angles = torch.atan2(rotor_pos_b[..., 1], rotor_pos_b[..., 0])
+
+        def get_template(tensor):
+            return tensor.flatten(0, -2)[0]
+
+        rotor_direction = get_template(self.rotor.rotor_direction)
+        moment_to_force = get_template(self.rotor.km_normalized / self.rotor.kf_normalized)
+
+        print(f'[INFO]: arm_lengths: {arm_lengths.tolist()}')
+        print(f'[INFO]: rotor_angles: {rotor_angles.tolist()}')
+        print(f'[INFO]: rotor_direction: {rotor_direction.tolist()}')
+
+        with torch.device(self.device):
+            self.vel_gain = torch.as_tensor(vel_gain)
+            self.z_gain = torch.as_tensor(z_gain)
+            gravity_dir, gravity_mag = self.asset.env.sim.get_physics_context().get_gravity()
+            self.gravity = torch.as_tensor(gravity_dir) * gravity_mag
+
+            I = torch.as_tensor([*inertia, 1]).diag_embed()
+            A = torch.stack(
+                [
+                    torch.sin(rotor_angles) * arm_lengths,
+                    -torch.cos(rotor_angles) * arm_lengths,
+                    -rotor_direction * moment_to_force,
+                    torch.ones(self.rotor.shape[-1])
+                ]
+            )
+            self.mixer = A.T @ (A @ A.T).inverse() @ I
+            self.body_rate_gain = torch.as_tensor(ang_rate_gain) @ I[:3, :3].inverse()
+            self.attitude_gain = torch.as_tensor(attitude_gain) @ I[:3, :3].inverse()
+
+        self.mass = get_template(
+            self.asset.root_physx_view
+            .get_masses()
+            .sum(-1, keepdim=True)
+            .to(self.device)
+        )
+
+    @property
+    def action_shape(self):
+        return torch.Size([*self.asset.shape, self.action_dim])
+
+    def apply_action(self, action: torch.Tensor):
+        batch_shape = action.shape[:-1]
+        action = action.reshape(-1, 4)
+        target_throttle = self._compute(action)
+        self.rotor.throttle_target[:] = target_throttle.reshape(*batch_shape, self.rotor.num_rotors)
+
+    def _compute(self, action: torch.Tensor):
+        target_roll, target_pitch, target_yaw, target_z = action.split([1, 1, 1, 1], dim=-1)
+
+        R = quaternion_to_rotation_matrix(self.asset.data.root_quat_w)
+
+        if self.z_mode == "force":
+            target_thrust = target_z
+        elif self.z_mode == "height":
+            pos = self.asset.data.root_pos_w - self.env.scene.env_origins
+            vel = self.asset.data.root_lin_vel_w
+
+            target_xy = pos[:, :2]
+            target_pos = torch.cat((target_xy, target_z), dim=1)
+
+            xy_gain = torch.zeros(target_xy.size(), device=self.device)
+            z_gain = torch.ones(target_z.size(), device=self.device) * self.z_gain
+            pos_gain = torch.cat((xy_gain, z_gain), dim=1)
+
+            pos_error = target_pos - pos
+            vel_error = 0. - vel
+            acc = (
+                + pos_error * pos_gain
+                + vel_error * self.vel_gain
+                - self.gravity
+            )
+            target_thrust = self.mass * (acc * R[:, :, 2]).sum(-1, True)
+        else:
+            raise ValueError(f"Invalid z mode: {self.z_mode}")
+
+        if self.yaw_mode == "angle":
+            yaw = axis_angle_to_matrix(target_yaw, torch.tensor([0., 0., 1.], device=self.device))
+        elif self.yaw_mode == "rate":
+            yaw = torch.atan2(R[:, 1, 0], R[:, 0, 0]).unsqueeze(-1)
+            yaw = axis_angle_to_matrix(yaw, torch.tensor([0., 0., 1.], device=self.device))
+        else:
+            raise ValueError(f"Invalid yaw mode: {self.yaw_mode}")
+
+        roll = axis_angle_to_matrix(target_roll, torch.tensor([1., 0., 0.], device=self.device))
+        pitch = axis_angle_to_matrix(target_pitch, torch.tensor([0., 1., 0.], device=self.device))
+        R_des = torch.bmm(torch.bmm(yaw,  roll), pitch)
+
+        ang_error_matrix = 0.5 * (
+            torch.bmm(R.transpose(-2, -1), R_des)
+            - torch.bmm(R_des.transpose(-2, -1), R)
+        )
+        ang_error = torch.stack([
+            ang_error_matrix[:, 2, 1],
+            ang_error_matrix[:, 0, 2],
+            ang_error_matrix[:, 1, 0]
+        ],dim=-1)
+
+        ang_vel = self.asset.data.root_ang_vel_w
+        body_rate = self.asset.data.root_ang_vel_b
+
+        if self.yaw_mode == "angle":
+            body_rate_err = 0. - body_rate
+        elif self.yaw_mode == "rate":
+            target_body_rate = torch.zeros_like(body_rate)
+            target_body_rate[:, 2] = target_yaw.squeeze(1)
+            body_rate_err = torch.bmm(torch.bmm(R_des.transpose(-2, -1), R), target_body_rate.unsqueeze(2)).squeeze(2) - body_rate
+        else:
+            raise ValueError(f"Invalid yaw mode: {self.yaw_mode}")
+
+        ang_acc = (
+            + ang_error * self.attitude_gain
+            + body_rate_err * self.body_rate_gain
+            + torch.cross(ang_vel, ang_vel, 1)
+        )
+        ang_acc_thrust = torch.cat([ang_acc, target_thrust], dim=-1)
+
+        target_thrusts = (self.mixer @ ang_acc_thrust.T).T
+        target_throttle = (target_thrusts / self.rotor.kf_normalized).sqrt()
+        return target_throttle
+
+    def debug_vis(self):
+        rotor_pos_w = self.asset.data.body_pos_w[..., self.rotor.body_ids, :].flatten(0, -2)
+        rotor_quat_w = self.asset.data.body_quat_w[..., self.rotor.body_ids, :].flatten(0, -2)
+
+        throttle = torch.zeros(self.rotor.shape + (3,), device=self.device)
+        throttle[..., 2] = self.rotor.throttle
+        throttle_w = quat_rotate(rotor_quat_w, throttle.flatten(0, -2))
+        self.env.debug_draw.vector(
+            rotor_pos_w,
+            throttle_w,
+        )
+
+        rest_throttle = torch.zeros(self.rotor.shape + (3,), device=self.device)
+        rest_throttle[..., 2] = 1.0 - self.rotor.throttle
+        rest_throttle_w = quat_rotate(rotor_quat_w, rest_throttle.flatten(0, -2))
+        self.env.debug_draw.vector(
+            rotor_pos_w + throttle_w,
+            rest_throttle_w,
+            color=(0., 0., 0., .5)
+        )
+
